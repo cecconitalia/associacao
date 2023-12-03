81aa19dbb27ba86bafa07e9dabc5a32ba9041e70
cecconitalia <152780540+cecconitalia@users.noreply.github.com>
2023-12-03 16:21:04 -0300

    Criação tabela leads

--- associacao/app_associacao/models.py
+++ associacao/app_associacao/models.py
@@ -5,3 +5,10 @@ class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
     idade = models.CharField(max_length=300)    
+
+class Lead(models.Model):
+    id_lead = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    email = models.EmailField()    
+    telefone = models.IntegerField()
+    assunto = models.TextField()
